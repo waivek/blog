2fd723a6bc42d70ecafa93cdb7b5cfbbbe5f41a8
Diksha Vashisth <dvdiksha@gmail.com>
2016-11-27 19:01:31 +0530

    Added styles. Completed upto a 2 command step
    After executing two commands, from pandoc then python, we get the output
    in an HTML file.

--- convert.py
+++ convert.py
@@ -1,14 +1,22 @@
 
-from markdown import markdown
 from bs4 import BeautifulSoup
+import sys
+import io
 
-doc = open("index.md", encoding="utf-8").read()
 
-html=markdown(doc)
+permalink='<a href="#para%d">permalink</a>'
+
+html=open("final.html", encoding="utf-8").read()
+
 soup=BeautifulSoup(html, "lxml")
 
-footnotes = {}
-for i, a in enumerate(soup("a")):
-    key = "#ref_" + str(i)
-    footnotes[key]=a["href"]
-    a["href"]=key
+for i, p in enumerate(soup.select("div.c1 > p")):
+    lnk = permalink % (i+1)
+    new_a = BeautifulSoup(lnk, "lxml").a
+    p['id']="para%d" % (i+1)
+    p.append(new_a)
+
+with open("test.html", "wb") as f:
+    f.write(soup.prettify("utf-8"))
+
+print("Done")
--- index.md
+++ index.md
@@ -1,10 +1,10 @@
 How do people get new ideas?
 ============================
 
-Presumably, the process of creativity, whatever it is, is essentially
+Presumably, the process[Something](https://something.com) of creativity, whatever it is, is essentially
 the same in all its branches and varieties, so that the evolution of a
 new art form, a new gadget, a new scientific principle, all involve
-common factors ^[https://google.com] . We are most interested in the
+common factors ^[[Google](https://google.com)] . We are most interested in the
 “creation” of a new scientific principle or a new application of an old
 one, but we can be general here. 
 
